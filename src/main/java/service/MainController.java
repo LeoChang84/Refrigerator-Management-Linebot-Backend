816be625f0c6a51e1ac38f5188257068ad881796
leo.chang <leochang9384@gmail.com>
2018-08-09 00:13:05 +0800

    first commit

---
+++ src/main/java/service/MainController.java
@@ -0,0 +1,14 @@
+package service;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @RequestMapping("/")
+    String hello() {
+        return "hello world";
+    }
+}
