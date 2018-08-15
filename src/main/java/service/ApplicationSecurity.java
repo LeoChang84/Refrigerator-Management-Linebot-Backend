40254d7e175405f0840ad33ae38dd775a2eb9b14
Leo-iMac <leo.chang@linecorp.com>
2018-08-15 21:16:59 +0800

    add broken corsFilter

--- src/main/java/service/ApplicationSecurity.java
+++ src/main/java/service/ApplicationSecurity.java
@@ -1,5 +1,6 @@
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+package service;
+
+import service.filter.CorsFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,21 +11,15 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-@Order(1)
 public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
-
-    @Value("${domain.name}")
-    private String DOMAIN_NAME;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+        http.cors().disable();
     }
     @Bean
     public FilterRegistrationBean corsFilterRegistration() {
-        FilterRegistrationBean registration = new FilterRegistrationBean<>(new CorsFilter(DOMAIN_NAME));
+        FilterRegistrationBean registration = new FilterRegistrationBean<>(new CorsFilter());
         registration.setOrder(1);
         return registration;
     }
--- src/main/java/service/filter/CorsFilter.java
+++ src/main/java/service/filter/CorsFilter.java
@@ -1,4 +1,4 @@
-package service;
+package service.filter;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
@@ -7,28 +7,23 @@ import java.io.IOException;
 import org.springframework.http.HttpMethod;
 
 public class CorsFilter implements Filter {
-    private String domainName;
     public void destroy() {
     }
-    public CorsFilter(String domainName) {
-        this.domainName = domainName;
-    }
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
-        HttpServletResponse res = (HttpServletResponse) response;
-        if (null == this.domainName || this.domainName.isEmpty()) {
-            res.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            return;
-        }
-        res.setHeader("Access-Control-Allow-Origin", this.domainName);
-        //res.setHeader("Access-Control-Allow-Origin", "*");
-        if (((HttpServletRequest)request).getMethod().equalsIgnoreCase(HttpMethod.OPTIONS.toString())) {
-            res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
-            res.setHeader("Access-Control-Max-Age", "1728000");
-            res.setHeader("Access-Control-Allow-Headers", "Content-Type, X-Authorization");
-            res.setStatus(HttpServletResponse.SC_OK);
-            return;
-        }
-        chain.doFilter(request, res);
+    public CorsFilter() { }
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws ServletException, IOException {
+
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        HttpServletRequest request= (HttpServletRequest) servletRequest;
+
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "*");
+        response.setHeader("Access-Control-Allow-Headers", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Max-Age", "1728000");
+        if (request.getMethod().equals("OPTIONS"))
+            response.setStatus(HttpServletResponse.SC_OK);
+        else
+            chain.doFilter(request, response);
     }
     public void init(FilterConfig config) throws ServletException {
     }
